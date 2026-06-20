2f43d3bb24ee610148ca6d6ec9a28562e9b1070c
molmuripranavi <molmuripranavi@gmail.com>
2026-06-20 10:02:02 +0530

    toggle

---
+++ frontend/js/toggle.js
@@ -0,0 +1,23 @@
+var users = [
+    {
+        "name": "John Doe",
+        "gender": "male",
+        "image": "../images/john.png"
+    },
+    {
+        "name": "Jane Doe",
+        "gender": "female",
+        "image": "../images/jane.png" 
+    }
+]
+var curId = 0;
+function toggle(){
+    //toggle curId from 0 to 1 and 1 to 0
+    curId = (curId+1)%2;
+    var user = users[curId];
+    document.getElementById("user-img").src = user.image;
+
+    // to do : update for name and gender !
+    document.getElementById("user-name").innerText = user.name;
+    document.getElementById("user-gender").innerText = user.gender;
+}
